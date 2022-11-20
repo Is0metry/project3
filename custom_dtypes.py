9ee56daadc2907ab9960a5bda5f9da1d90edf402
Woody Sims <sims.woodrow@gmail.com>
2022-11-20 16:08:59 -0600

    created custom dtypes for function annotation

---
+++ custom_dtypes.py
@@ -0,0 +1,10 @@
+'''contains the data type aliases used in typing of each function for docstrings'''
+import typing as t
+
+import pandas as pd
+from numpy.typing import ArrayLike
+from sklearn.linear_model import LassoLars, LinearRegression, TweedieRegressor
+
+ModelDataType = t.Union[ArrayLike,pd.DataFrame,pd.Series]
+LinearRegressionType = t.Union[LinearRegression,LassoLars,TweedieRegressor]
+PandasDataType = t.Union[pd.Series,pd.DataFrame]
